f31554ae56a85c2e8776c20a8953e4c19cc17860
per1994 <patrick.schmelzle@t-online.de>
2015-06-21 16:49:43 +0200

    benutzer erweitert
    erste variablen eingefügt

--- src/main/java/org/plp/benutzer/Benutzer.java
+++ src/main/java/org/plp/benutzer/Benutzer.java
@@ -1,5 +1,17 @@
 package org.plp.benutzer;
 
+import java.util.ArrayList;
+
+import org.plp.gruppenfunktionen.Moderator;
+
 public class Benutzer {
+	
+	Profil profil= new Profil();
+	private String password;
+	String email;
+	ArrayList freundesliste = new ArrayList();
+	Moderator moderator= new Moderator();
+
+	
 
 }
