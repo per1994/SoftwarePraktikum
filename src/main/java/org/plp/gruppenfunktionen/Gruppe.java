3a2677b23b6624a59855833a4bd24a6d47e2b879
st109898 <pascal.maichl1@web.de>
2015-06-22 10:24:56 +0200

    Gruppe fast Fertig
    Gruppen Getter und Setter
    
    Sükrü stinkt

--- src/main/java/org/plp/gruppenfunktionen/Gruppe.java
+++ src/main/java/org/plp/gruppenfunktionen/Gruppe.java
@@ -1,76 +1,100 @@
 package org.plp.gruppenfunktionen;
 
+import org.plp.benutzer.*;
+import org.plp.gamification.*;
 import java.util.ArrayList;
 
 public class Gruppe {
 
-	private ArrayList mitgliederListe = new ArrayList ();
-	private ArrayList moderatorenListe = new ArrayList ();
+	private ArrayList<Benutzer> mitgliederListe = new ArrayList<Benutzer> ();
+	private ArrayList<Moderator> moderatorenListe = new ArrayList<Moderator> ();
 	private int anzahlMitglieder = 0;
 	private Fachrichtung fachrichtung = new Fachrichtung();
 	private double lernFortschritt = 0.0;
 	private String name;
 	private int id = 0;
 	private String passwort;
-	private ArrayList lernziele = new ArrayList();
+	private ArrayList<Lernziel> lernziele = new ArrayList<Lernziel>();
 	private Lernziel lernziel = new Lernziel();
-	public ArrayList getMitgliederListe() {
+	
+	
+	public ArrayList<Benutzer> getMitgliederListe() {
 		return mitgliederListe;
 	}
-	public void setMitgliederListe(ArrayList mitgliederListe) {
-		this.mitgliederListe = mitgliederListe;
+	
+	public void addMitgliedToMitgliederListe(Benutzer benutzer) {
+		mitgliederListe.add(benutzer);
 	}
-	public ArrayList getModeratorenListe() {
+	
+	public ArrayList<Moderator> getModeratorenListe() {
 		return moderatorenListe;
 	}
-	public void setModeratorenListe(ArrayList moderatorenListe) {
-		this.moderatorenListe = moderatorenListe;
+	
+	public void addModeratorToModeratorenListe(Moderator moderator) {
+		moderatorenListe.add(moderator);
 	}
+	
 	public int getAnzahlMitglieder() {
 		return anzahlMitglieder;
 	}
+	
 	public void setAnzahlMitglieder(int anzahlMitglieder) {
 		this.anzahlMitglieder = anzahlMitglieder;
+		Moderator auch in MitgliederListe? sonst Länge + Länge
 	}
+	
 	public Fachrichtung getFachrichtung() {
 		return fachrichtung;
 	}
+	
 	public void setFachrichtung(Fachrichtung fachrichtung) {
 		this.fachrichtung = fachrichtung;
 	}
+	
 	public double getLernFortschritt() {
 		return lernFortschritt;
 	}
+	
 	public void setLernFortschritt(double lernFortschritt) {
 		this.lernFortschritt = lernFortschritt;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public int getId() {
 		return id;
 	}
+	
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 	public String getPasswort() {
 		return passwort;
 	}
+	
 	public void setPasswort(String passwort) {
 		this.passwort = passwort;
 	}
-	public ArrayList getLernziele() {
+	
+	public ArrayList<Lernziel> getLernziele() {
 		return lernziele;
 	}
-	public void setLernziele(ArrayList lernziele) {
-		this.lernziele = lernziele;
+	
+	public void addLernzielToLernziele(Lernziel lernziel) {
+		lernziele.add(lernziel);
 	}
+	
 	public Lernziel getLernziel() {
 		return lernziel;
 	}
+	
 	public void setLernziel(Lernziel lernziel) {
 		this.lernziel = lernziel;
 	}
