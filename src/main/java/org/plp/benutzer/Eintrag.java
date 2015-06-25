1d3d85d4978fdaf738b04d452d4d13797ad419eb
per1994 <patrick.schmelzle@t-online.de>
2015-06-25 12:56:39 +0200

    Revert "neuer stand"
    This reverts commit 751aa352d19025f0f7ab376b555bfe79cf3b6b72.

--- src/main/java/org/plp/benutzer/Eintrag.java
+++ src/main/java/org/plp/benutzer/Eintrag.java
@@ -1,24 +1,5 @@
 package org.plp.benutzer;
 
-import java.util.ArrayList;
-
 public class Eintrag {
-	
-	String eintragstext;
-	ArrayList<String> kommentare;
-	
-	
-	public String getEintragstext() {
-		return eintragstext;
-	}
-	public void setEintragstext(String eintragstext) {
-		this.eintragstext = eintragstext;
-	}
-	public ArrayList<String> getKommentare() {
-		return kommentare;
-	}
-	public void setKommentare(ArrayList<String> kommentare) {
-		this.kommentare = kommentare;
-	}
 
 }
