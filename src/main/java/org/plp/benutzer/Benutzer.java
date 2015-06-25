f15a516b4af299b37dcb635ff100cf89d0fd6ce1
per1994 <patrick.schmelzle@t-online.de>
2015-06-25 12:57:53 +0200

    Revert "benutzer erweitert"
    This reverts commit 8f4dfb546d85cf2e861f3316d9728059c0beb839.

--- src/main/java/org/plp/benutzer/Benutzer.java
+++ src/main/java/org/plp/benutzer/Benutzer.java
@@ -1,17 +1,5 @@
 package org.plp.benutzer;
 
-import java.util.ArrayList;
-
-import org.plp.gruppenfunktionen.Moderator;
-
 public class Benutzer {
-	
-	Profil profil= new Profil();
-	private String password;
-	String email;
-	ArrayList freundesliste = new ArrayList();
-	Moderator moderator= new Moderator();
-
-	
 
 }
