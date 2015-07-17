5c56edf6cee876e5e708468105c497ec52763cb4
per1994 <patrick.schmelzle@t-online.de>
2015-07-17 14:47:48 +0200

    Revert "nochmal dao"
    This reverts commit 40df1344e0eebe37a99b09aa1dd15d65006cb6e6.

--- src/main/java/org/plp/dao/BenutzerDAOImpl.java
+++ src/main/java/org/plp/dao/BenutzerDAOImpl.java
@@ -56,7 +56,7 @@ public class BenutzerDAOImpl implements BenutzerDAO {
 
 	@Override
 	public Benutzer getBenutzer(int benutzer_id) {
-		Benutzer benutzer = (Benutzer) sessionFactory.getCurrentSession().get(
+		Benutzer benutzer = (Benutzer) sessionFactory.getCurrentSession().load(
                 Benutzer.class, benutzer_id);
 		return benutzer;
 	}
